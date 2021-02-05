b1a9b023f873e932f559136248cd8af7b5447dd2
Chenerywoman <rachelmchenery@hotmail.com>
2021-02-05 15:20:36 +0000

    correcting previous commit - solution with loop

--- keyboard_challenge.js
+++ keyboard_challenge.js
@@ -1,36 +1,69 @@
 // challenge at codewars: https://www.codewars.com/kata/5a5032f4fd56cb958e00007a/javascript
 
-const TVRemote = (word) => {
+ const tvRemote = function(word) {
+  
+  const keyBoard = {
+    a: [0, 0],
+    b: [0, 1],
+    c: [0, 2],
+    d: [0, 3],
+    e: [0, 4], 
+    1: [0, 5],
+    2: [0, 6],
+    3: [0, 7],
+    f: [1, 0],
+    g: [1, 1],
+    h: [1, 2],
+    i: [1, 3],
+    j: [1, 4],
+    4: [1, 5],
+    5: [1, 6],
+    6: [1, 7],
+    k: [2, 0],
+    l:[2, 1],
+    m:[2, 2],
+    n:[2, 3],
+    o:[2, 4],
+    7:[2, 5],
+    8:[2, 6],
+    9:[2, 7],
+    p:[3, 0],
+    q:[3, 1],
+    r:[3, 2],
+    s:[3, 3],
+    t:[3, 4],
+    '.':[3, 5],
+    '@':[3, 6],
+     0:[3,7],
+     u:[4, 0],
+     v:[4, 1],
+     w:[4, 2],
+     x:[4, 3],
+     y:[4,4],
+     z:[4,5],
+     '_':[4,6],
+     '/':[4,7]
+  }
+  
+  let count=0;
+  
+  const step1 = (Math.abs(0-keyBoard[word[0]][0])) + Math.abs((0-keyBoard[word[0]][1])) + 1;
+    console.log(step1);
+  count = count + step1;
 
-const keyBoard = [
-    ['a', 'b', 'c', 'd', 'e', 1, 2, 3],
-    ['f', 'g', 'h', 'i', 'j', 4, 5, 6],
-    ['k', 'l', 'm', 'n', 'o', 4, 5, 6],
-]
-
-let presses = 0;
-
-presses += word.length;  // Ok for each character
-
-const letters = [...word];
-
-const getPosition(character) => {
-    let rowPosition 
+  for(let i=0; i<word.length-1;i++){
+     let iIndex=Math.abs(keyBoard[word[i]][0]-keyBoard[word[i+1]][0]);
+     let jIndex=Math.abs(keyBoard[word[i]][1]-keyBoard[word[i+1]][1]);
+      let step2=iIndex+jIndex;
+    step2=step2+1;
+    count = count+step2;
+    console.log(step2);
+  }
+  
+  return count;
 }
+Best Practices0Clever0
+0ForkCompare with your solutionLink
+© 2021 CodewarsAboutAPIBlogPrivacyTermsContact
+powered by
 
-const startingCharacter = 'a';
-
-const startingCharacterRow = keyBoard.findIndex(row => row.includes(startingCharacter));
-
-const startingCharacterColumn = keyBoard[startingCharacterRow].findIndex(item => item === startingCharacter);
-
-const targetRow = keyboard.findIndex(row => row.includes(word));
-const targetColumn = keyBoard[targetRow].findIndex(item => item === word);
-
-const horizontalMoves = targetColumn - startingCharacterColumn;
-const verticalRows = targetRow - startingCharacterRow;
-
-presses = presses + horizontalMoves + verticalMoves;
-
-return presses;
-}
